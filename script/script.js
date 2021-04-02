26d5e803530a595dbc4b5e0cc7455ba09534c379
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-02 19:44:15 +0300

    Home Task2 Complex: insert let i=0; in to FOR cycle body

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
 const num = 266219;
 const num_str = String(num);
 let result = 1;
-let i;
 
-for (i = 0; i < num_str.length; i++) {
+for (let i = 0; i < num_str.length; i++) {
     result *= num_str[i];
 }
 console.log(result);
