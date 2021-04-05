b085d33d2b2d7e36bc3d0f8be27464c7cccadcf5
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-05 19:11:11 +0300

    lesson03

--- script/script.js
+++ script/script.js
@@ -1,17 +1,30 @@
-const num = 266219;
-const numStr = String(num);
-let result = 1;
+let lang = 'en';
+const enWeek = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+const ruWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+const ruenWeekDays = {
+    ru: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    en: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+};
 
-for (let i = 0; i < numStr.length; i++) {
-    result *= numStr[i];
+if (lang === 'ru') {
+    ruWeek.forEach((day) => { console.log(day); });
+} else {
+    enWeek.forEach((day) => { console.log(day); });
 }
-console.log(result);
-result = result ** 3;
-console.log(result);// в задании этого нет ( но без этого как то не наглядно)
-console.log(String(result).substr(0, 2));
 
+switch (lang) {
+    case 'ru':
+        ruWeek.forEach((day) => { console.log(day); });
+        break;
+    case 'en':
+        enWeek.forEach((day) => { console.log(day); });
+}
 
+ruenWeekDays[lang].forEach((day) => { console.log(day); });
 
+let namePerson = 'Максим';
+
+(namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
 
 
 
