9bc7278524766d62cc5e410add1d732fdb9c6afd
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-06 09:17:41 +0300

    Lesson 04 Complex

--- script/script.js
+++ script/script.js
@@ -1,35 +1,13 @@
-let lang = 'en';
-const enWeek = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-const ruWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-const ruenWeekDays = {
-    ru: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-    en: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+"use strict";
+
+const treeDots = function (arg) {
+    if (typeof (arg) === "string") {
+        arg = arg.trim();
+        return arg.length > 30 ? arg.slice(0, 29) + '...' : arg;
+    } else {
+        alert('Переданный в функцию treeDots аргумент не является строкой');
+    }
+
 };
-
-if (lang === 'ru') {
-    ruWeek.forEach((day) => { console.log(day); });
-} else {
-    enWeek.forEach((day) => { console.log(day); });
-}
-
-switch (lang) {
-    case 'ru':
-        ruWeek.forEach((day) => { console.log(day); });
-        break;
-    case 'en':
-        enWeek.forEach((day) => { console.log(day); });
-}
-
-ruenWeekDays[lang].forEach((day) => { console.log(day); });
-
-let namePerson = 'Максим';
-
-(namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
-
-
-
-
-
-
-
-
+// чисто для проверки
+console.log(treeDots('          qjhfgwjqhfjekhgfjkwqhfgqjwkhgfkjwqhfgjkwqhfgjqwkhegfjqwhgefwqjhefjwhefhhhhh               ') + '4555');
