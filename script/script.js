82ec8de68674f6747e074a5686acc6012bbb5c37
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-07 09:29:35 +0300

    Lesson05 Complex

--- script/script.js
+++ script/script.js
@@ -1,13 +1,34 @@
 "use strict";
 
-const treeDots = function (arg) {
-    if (typeof (arg) === "string") {
-        arg = arg.trim();
-        return arg.length > 30 ? arg.slice(0, 29) + '...' : arg;
-    } else {
-        alert('Переданный в функцию treeDots аргумент не является строкой');
+const arr = ['235', '335', '735', '435', '186', '678', '455'];
+
+const out2or4 = (arr1) => {
+    for (let i = 0; i < arr1.length; i++) {
+        if (parseInt(arr1[i].slice(0, 1)) === 2 || parseInt(arr1[i].slice(0, 1)) === 4) {
+            console.log(arr1[i]);
+        }
     }
 
+
 };
-// чисто для проверки
-console.log(treeDots('          qjhfgwjqhfjekhgfjkwqhfgqjwkhgfkjwqhfgjkwqhfgjqwkhegfjqwhgefwqjhefjwhefhhhhh               ') + '4555');
+
+out2or4(arr);
+
+console.log("End of Part 1");
+
+const isSimple = (n) => {
+    let result = true;
+    for (let i = n - 1; i >= 1; i--) {
+        if ((n % i === 0) && (i !== 1)) {
+            result = false; break;
+        }
+    }
+    return result;
+};
+
+for (let num = 1; num <= 100; num++) {
+
+    if (isSimple(num)) {
+        console.log('простое', num);
+    }
+}
