490faa0840fb4a1b7a58062b4b6aa5339e6361dc
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-09 19:43:15 +0300

    HT7 Complex

--- script/script.js
+++ script/script.js
@@ -1,45 +1,53 @@
 "use strict";
+const isNumber = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-const arr = ['235', '335', '735', '435', '186', '678', '455'];
+const startInputNumber = (mess) => {
+    let num1 = prompt(mess);
+    if (num1 === null) {
+        return null;
+    }
+    if (!isNumber(+num1)) {
+        return startInputNumber("Введите число!");
+    }
+    return +num1;
+};
 
-const out2or4 = (arr1) => {
-    for (let i = 0; i < arr1.length; i++) {
-        if (parseInt(arr1[i].slice(0, 1)) === 2 || parseInt(arr1[i].slice(0, 1)) === 4) {
-            console.log(arr1[i]);
+const getGame = () => {
+    let x = Math.floor(Math.random() * 99 + 1);
+    console.log("x: ", x);
+    let y = 10;
+    const game = (mes) => {
+        if (y--) {
+            let num = startInputNumber(mes);
+            if (num === null) {
+                alert("Игра окончена");
+                return 0;
+            }
+            if (num === x) {
+                return 1;
+            }
+            if (num > x) {
+                return game("Загаданное число меньше, осталось " + y + " попыток");
+            }
+            return game("Загаданное число больше, осталось " + y + " попыток");
+        }
+        return 2;
+    };
+    return game;
+};
+
+
+const upGame = () => {
+    const myGame = getGame();
+    let resultGame = myGame("Введите число от 1 до 100");
+    if (resultGame > 0) {
+        let mess2 = resultGame === 1 ? "Вы угадали!" : "Попытки закончились!";
+        if (confirm(mess2 + " Сыграем еще?")) {
+            return upGame();
         }
     }
-
-
 };
 
-out2or4(arr);
-
-console.log("End of Part 1");
-
-const isSimple = (n) => {
-
-    if (n === 1 || n === 4) {
-        return false;
-    }
-
-    if (n === 2 || n === 3) {
-        return true;
-    }
-
-    if (n % 2 === 0 || n % 3 === 0) {
-        return false;
-    }
-
-    for (let k = 3; k <= n / 3 + 1; k += 2) {
-        if (n % k === 0) { return false; }
-    }
-    return true;
-};
-
-for (let num = 1; num <= 10000; num++) {
-
-    if (isSimple(num)) {
-        console.log('Простое число ' + num + ' делится на 1 и на ' + num + '.');
-    }
-}
-
+upGame();
