ee244df8daf3ca543f729a708f9459185672d4d2
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-11 15:04:01 +0300

    HT7 complex

--- script/script.js
+++ script/script.js
@@ -1,53 +1,21 @@
 "use strict";
-const isNumber = (n) => {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const rusDayOfWeek = (n) => {
+    return (n === 0) ? 6 : n--;
+};
+const boldWarp = (s) => {
+    return "<b>" + s + "</b>";
+};
+const italicWarp = (s) => {
+    return "<i>" + s + "</i>";
 };
 
-const startInputNumber = (mess) => {
-    let num1 = prompt(mess);
-    if (num1 === null) {
-        return null;
-    }
-    if (!isNumber(+num1)) {
-        return startInputNumber("Введите число!");
-    }
-    return +num1;
-};
+let dayOfWeek = new Date();
+dayOfWeek = rusDayOfWeek(dayOfWeek.getDay()); // получаем текущий русский номер дня недели
 
-const getGame = () => {
-    let x = Math.floor(Math.random() * 99 + 1);
-    console.log("x: ", x);
-    let y = 10;
-    const game = (mes) => {
-        if (y--) {
-            let num = startInputNumber(mes);
-            if (num === null) {
-                alert("Игра окончена");
-                return 0;
-            }
-            if (num === x) {
-                return 1;
-            }
-            if (num > x) {
-                return game("Загаданное число меньше, осталось " + y + " попыток");
-            }
-            return game("Загаданное число больше, осталось " + y + " попыток");
-        }
-        return 2;
-    };
-    return game;
-};
-
-
-const upGame = () => {
-    const myGame = getGame();
-    let resultGame = myGame("Введите число от 1 до 100");
-    if (resultGame > 0) {
-        let mess2 = resultGame === 1 ? "Вы угадали!" : "Попытки закончились!";
-        if (confirm(mess2 + " Сыграем еще?")) {
-            return upGame();
-        }
-    }
-};
-
-upGame();
+const wbody = document.querySelector("body");
+for (let i = 0; i < 7; i++) {
+    let dayToSite = (i > 4) ? italicWarp(week[i]) : week[i];
+    dayToSite = (i === dayOfWeek) ? boldWarp(dayToSite) : dayToSite;
+    wbody.innerHTML += dayToSite + '<br>';
+}
