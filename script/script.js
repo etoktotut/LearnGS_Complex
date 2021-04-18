84d0017c944082bd3a02af8a756af70ae2c1f2d3
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-18 13:16:20 +0300

    HT13 complex

--- script/script.js
+++ script/script.js
@@ -1,57 +1,11 @@
 "use strict";
-let week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-let month = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+const wbody = document.querySelector('body');
+const h1Here = document.querySelector('h1');
+const btnChange = document.querySelector('#change');
 
-const minSecEnding = (n) => {
-    if ((n > 0 && n < 5) || (n > 20)) {
-        n = n % 10;
-        if (n === 1) {
-            return "а";
-        }
-        if (n > 1 && n < 5) {
-            return "ы";
-        }
-    }
-    return "";
-}
+btnChange.addEventListener('click', function () {
+    let randColor = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+    wbody.setAttribute('style', `background-color: ${randColor};`);
+    h1Here.textContent = randColor;
+});
 
-
-const endOfHour = (hour) => {
-    if (hour === 1 || hour === 21) {
-        return "";
-    }
-    if ((hour > 1 && hour < 5) || (hour > 21 && hour < 24)) {
-        return "а";
-    }
-    return "ов";
-};
-
-const twoSymbolConversion = (z) => {
-    let str = String(z);
-    if (z < 10) {
-        str = "0" + str;
-    }
-    return str;
-};
-
-
-
-const wbody = document.querySelector("body");
-wbody.innerHTML += '<div class="short_time">< -/div>';
-const shortTime = document.querySelector('.short_time');
-shortTime.style.color = 'red';
-shortTime.style.fontFamily = 'Arial';
-shortTime.style.fontWeight = 'bold';
-
-const shortTimeUpdate = () => {
-    let currDate = new Date();
-    shortTime.innerHTML = "";
-    shortTime.innerHTML += "Сегодня " + week[currDate.getDay()] + ", " + currDate.getDate() + " " + month[currDate.getMonth()] + " " + currDate.getFullYear() + " года, ";
-    shortTime.innerHTML += currDate.getHours() + ' час' + endOfHour(currDate.getHours()) + " ";
-    shortTime.innerHTML += currDate.getMinutes() + ' минут' + minSecEnding(currDate.getMinutes()) + " " + currDate.getSeconds() + " секунд" + minSecEnding(currDate.getSeconds()) + "<br>";
-    shortTime.innerHTML += twoSymbolConversion(currDate.getDate()) + ":" + twoSymbolConversion(currDate.getMonth() + 1) + ":" + currDate.getFullYear() + " - ";
-    shortTime.innerHTML += twoSymbolConversion(currDate.getHours()) + ":" + twoSymbolConversion(currDate.getMinutes()) + ":" + twoSymbolConversion(currDate.getSeconds());
-};
-
-shortTimeUpdate();
-let sInt = setInterval(shortTimeUpdate, 1000);
